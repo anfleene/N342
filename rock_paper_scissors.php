123e568933bc28488c10f81026fcfa07bbd3f61e
Andy Fleener <anfleene@iupui.edu>
2010-03-03 14:21:23 -0500

    finished easy version of rock paper scissors

--- rock_paper_scissors.php
+++ rock_paper_scissors.php
@@ -7,10 +7,13 @@
   <div id="errorFlash" class="error" style="display:none"></div>
   <?php
     printForm();
-    if($userGuess = filter_input(INPUT_POST, userGuess)){
+    $userGuess = filter_input(INPUT_POST, "userGuess");
+    if(isset($userGuess)){
       $compGuess = CompGuess();
-      updateScores($userGuess, $compGuess);
+      $winner = updateScores($userGuess, $compGuess);
+      printResults($userGuess, $compGuess, $winner);
     }
+    
     
   ?>
 </div>
@@ -29,11 +32,47 @@ function updateScores($userGuess, $compGuess){
   $beats = array(1,2,0);
   if($userGuess == $compGuess){
     $_SESSION['TIE']++;
+    $winner = "Tie";
   }else if($beats[$userGuess] == $compGuess){
     $_SESSION['LOSS']++;
-  }else
-    $_SESSION['WIN']++;
+    $winner = "Computer";
+  }else{
+   $_SESSION['WIN']++;
+   $winner = "Human"; 
+  }
+  return $winner;
 }
+
+function percent($key){
+  $total = $_SESSION['WIN'] ? $_SESSION['WIN'] : 0;
+  $total += $_SESSION['LOSS'] ? $_SESSION['LOSS'] : 0;
+  $total += $_SESSION['TIE'] ? $_SESSION['TIE'] : 0;
+  return round($_SESSION[$key] ? ($_SESSION[$key] / $total * 100) : 0, 2);
+}
+
+function printResults($human, $comp, $winner ){
+  $guessName = array("Rock", "Paper", "Scissors")
+?>
+  <div>
+   <dl class="results">
+     <dt>Human Guess:</dt>
+      <dd><?= $guessName[$human]?></dd>
+     <dt>Computer Guess:</dt>
+      <dd><?= $guessName[$comp]?></dd>
+     <dt>Winner:</dt>
+      <dd><?= $winner?></dd>
+   </dl>
+   <dl class="results">
+     <dt>Human Wins:</dt>
+      <dd><?= $_SESSION['WIN'] ? $_SESSION['WIN'] : 0?>(<?= percent('WIN') ?>%)</dd>
+     <dt>Computer Wins:</dt>
+      <dd><?= $_SESSION['LOSS'] ? $_SESSION['LOSS'] : 0?>(<?= percent('LOSS') ?>%)</dd>
+     <dt>Ties:</dt>
+      <dd><?= $_SESSION['TIE'] ? $_SESSION['TIE'] : 0?>(<?= percent('TIE') ?>%)</dd>
+   </dl>
+   <??>
+  </div> 
+<?}
 function printForm(){?>
   <div class="update">
     <form action = "rock_paper_scissors.php" method ="post">
