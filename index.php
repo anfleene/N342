9e7786fdd37c3e0172f9e639faf8c60069344782
Andy Fleener <anfleene@iupui.edu>
2010-03-03 14:22:40 -0500

    update to index to include rps

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
       <div id="lab1" class="lab">
         <h3><a href="face_maker_form.php">Face Maker Lab</a></h3>
         <h3><a href="gradebook.php">GradeBook Lab</a></h3>
+        <h3><a href="rock_paper_scissors.php">Rock-Paper-Scissors Lab-Boring Version</a></h3>
       </div> 
     </div>
   </div>
